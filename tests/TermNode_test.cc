c7123a82f9a07a7bea5eb7251d3ebc08c4270ab9
Maxx Tepper <maxxtepper@gmail.com>
2020-03-27 17:31:31 -0700

    updated tests

---
+++ tests/TermNode_test.cc
@@ -0,0 +1,10 @@
+#include <memory>
+#include "gtest/gtest.h"
+#include "src/lib/TermNode.h"
+
+TEST(TermNodeShould, ReturnSomething){
+	std::unique_ptr<TermNode> termnode = std::make_unique<TermNode>();
+	std::string actual = termnode->DoSomething();
+	std::string expected = Something;
+	EXPECT_EQ(expected, actual);
+}
---
+++ tests/TrieNode_test.cc
@@ -0,0 +1,10 @@
+#include <memory>
+#include "gtest/gtest.h"
+#include "src/lib/TrieNode.h"
+
+TEST(TrieNodeShould, ReturnSomething){
+	std::unique_ptr<TrieNode> trienode = std::make_unique<TrieNode>();
+	std::string actual = trienode->DoSomething();
+	std::string expected = Something;
+	EXPECT_EQ(expected, actual);
+}
