51c3d422dd2b617a1ee56a0aa712b135c6b00d62
Maxx Tepper <maxxtepper@gmail.com>
2021-04-17 16:05:45 -0700

    changed test names for new Trie -- will need to fix the tests

--- tests/Trie_test_0.cc
+++ tests/Trie_test_0.cc
@@ -181,3 +181,27 @@ TEST(TrieShould, GetWords){
 	/*
 	*/
 }
+#include <memory>
+#include "gtest/gtest.h"
+#include "src/lib/TrieNode.h"
+
+TEST(TrieNodeShould, ReturnSomething){
+/*
+	std::unique_ptr<TrieNode> trienode = std::make_unique<TrieNode>();
+	std::string actual = trienode->DoSomething();
+	std::string expected = Something;
+	EXPECT_EQ(expected, actual);
+*/
+}
+#include <memory>
+#include "gtest/gtest.h"
+#include "src/lib/TrieNode.h"
+
+TEST(TrieNodeShould, ReturnSomething){
+/*
+	std::unique_ptr<TrieNode> trienode = std::make_unique<TrieNode>();
+	std::string actual = trienode->DoSomething();
+	std::string expected = Something;
+	EXPECT_EQ(expected, actual);
+*/
+}
