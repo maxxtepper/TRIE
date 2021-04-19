752680f753393f4b93df68cc2119080450edd090
Maxx Tepper <maxxtepper@gmail.com>
2021-04-18 18:38:23 -0700

    Restructure Proclamation: TrieNode no longer works on itself, it has an other object do that; Trie (on its way)

--- src/lib/TrieNode.cc
+++ src/lib/TrieNode.cc
@@ -1,6 +1,8 @@
 #include "TrieNode.h"
 
-TrieNode::TrieNode() {
+TrieNode::TrieNode(const char letter, const std::string word) {
+	letter_ = letter;
+	word_ = word;
 }
 
 TrieNode::~TrieNode() {
--- src/lib/TrieNode.h
+++ src/lib/TrieNode.h
@@ -1,12 +1,34 @@
 #ifndef TRIE_SRC_LIB_TRIENODE_H_
 #define TRIE_SRC_LIB_TRIENODE_H_
 
+#include <string>
+#include <cstdint>
+#include <algorithm>
+#include <memory>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+
+typedef std::vector<std::string> WordList;
+
 class TrieNode {
 	public:
-		TrieNode();
+		TrieNode(const char letter, const std::string word);
 		~TrieNode();
 
+		//	The children nodes
+		std::map<char, std::shared_ptr<TrieNode>> children;
+
+		//	Object Essentials
+		char GetLetter() { return letter_; }
+		std::string GetWord() { return word_; }
+
 	private:
+		//	The char of this node
+		char letter_;
+		//	The word up to this point
+		std::string word_;
 };
 
 #endif
